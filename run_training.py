049edcd3440f0dbca00658b69882b5142c9a61ef
callum-jpg <callumtc.jpg@gmail.com>
2022-12-20 20:58:41 +0000

    rearraging things a bit

---
+++ run_training.py
@@ -0,0 +1,157 @@
+import sys
+sys.path.append("../")
+import os
+import shutil
+import tempfile
+import time
+import numpy as np
+from monai.losses import DiceLoss
+from monai.inferers import sliding_window_inference
+
+from torch.utils.data import DataLoader, Dataset
+
+import torch
+torch.cuda.empty_cache()
+import torchvision
+import skimage
+import pandas as pd
+import sklearn
+import sklearn.model_selection
+
+from unet.augmentations.augmentations import (
+    Compose,
+    ToTensor,
+    LabelsToEdgesAndCentroids,
+    RandomContrastBrightness,
+    RandomGuassianBlur,
+    RandomGaussianNoise,
+    RandomRotate2D,
+    Flip,
+    RandomRot90,
+    RandomPoissonNoise,
+    ElasticDeform,
+    RandomScale,
+    Normalize
+)
+from unet.utils.load_data import MaddoxDataset
+from unet.networks.threedee_unet import UNet_3D
+
+load_csv = pd.read_csv("patch_data/load_data_training.csv")
+
+train_dataset, val_dataset = sklearn.model_selection.train_test_split(load_csv, test_size=0.2)
+
+train_ds = MaddoxDataset(
+    data_csv=train_dataset,
+    train_val="train"
+)
+
+val_ds = MaddoxDataset(
+    data_csv=val_dataset,
+    train_val="val"
+)
+
+train_loader = DataLoader(train_ds, batch_size=4, shuffle=True, collate_fn=train_ds.collate_fn)
+
+# Don't shuffle validation so you can see how predictions improve over time
+val_loader = DataLoader(val_ds, batch_size=4, shuffle=False, collate_fn=val_ds.collate_fn)
+
+def train_step(
+    model,
+    device,
+    data_loader,
+    loss_fn,
+    optimizer
+):
+    model.train()
+    for i, (X, y) in enumerate(data_loader):
+        # Put the image and the mask on the device
+        X, y = X.to(device), y.to(device)
+        # Clear gradients from the optimizer
+        optimizer.zero_grad()
+        # Run a prediction
+        prediction = model(X)
+        print(f"Prediction shape: {prediction.shape} --- Target shape: {y.shape}")
+        # Find the loss of the prediction when compared to the GT
+        loss = loss_fn(prediction, y)
+        # Using this loss, calculate the gradients of loss for all parameters
+        loss.backward()
+        # Update the the parameters of the model
+        optimizer.step()
+
+        # Sigmoid scales values between 0-1
+        probability = prediction.sigmoid().detach().cpu().numpy().flatten()
+        y = y.long().detach().cpu().numpy().flatten()
+
+        # roc = sklearn.metrics.roc_auc_score(y, probability, labels=[0, 1], multi_class="ovo")
+
+        # Assume that correct predictions are above threshold
+        # Use correctly classified pixels to calculate precision, recall, accuracy and f1
+        th_prediction = (probability > 0.5)
+        precision = sklearn.metrics.precision_score(y, th_prediction, labels=[0, 1])
+        recall = sklearn.metrics.recall_score(y, th_prediction, labels=[0, 1])
+        accuracy = sklearn.metrics.accuracy_score(y, th_prediction)
+        f1 = sklearn.metrics.f1_score(y, th_prediction, labels=[0, 1])
+
+        print(
+            f"""
+            Training step: {i} of {len(data_loader)}
+            Loss: {loss}
+            Accuracy: {accuracy}
+            Precision: {precision}
+            Recall: {recall}
+            F1: {f1}
+            """
+        )
+
+def validation_step(
+    model,
+    device,
+    data_loader,
+    loss_fn
+):
+    model.eval()
+    with torch.no_grad():
+        for i, (X, y) in enumerate(data_loader):
+            X, y = X.to(device), y.to(device)
+            prediction = model(X)
+            print(f"Prediction shape: {prediction.shape} --- Target shape: {y.shape}")
+            loss = loss_fn(prediction, y)
+
+            probability = prediction.sigmoid().detach().cpu().numpy().flatten()
+            y = y.long().cpu().detach().numpy().flatten()
+
+            # roc = sklearn.metrics.roc_auc_score(y, probability, labels=[0, 1], multi_class="ovo")
+            
+            th_prediction = (probability > 0.5)
+            precision = sklearn.metrics.precision_score(y, th_prediction, labels=[0, 1])
+            recall = sklearn.metrics.recall_score(y, th_prediction, labels=[0, 1])
+            accuracy = sklearn.metrics.accuracy_score(y, th_prediction)
+            f1 = sklearn.metrics.f1_score(y, th_prediction, labels=[0, 1])
+
+
+            print(
+                f"""
+                Validation step: {i} of {len(data_loader)}
+                Loss: {loss}
+                Accuracy: {accuracy}
+                Precision: {precision}
+                Recall: {recall}
+                F1: {f1}
+                """
+            )
+
+max_epochs = 2
+
+device = "cuda"
+model = UNet_3D(
+    in_channels=1, 
+    out_channels=2
+    ).to(device)
+
+loss_function = DiceLoss(smooth_nr=0, smooth_dr=1e-5, squared_pred=True, to_onehot_y=False, sigmoid=False)
+optimizer = torch.optim.Adam(model.parameters(), 1e-4, weight_decay=1e-5)
+
+for epoch in range(max_epochs):
+    print("epoch:", epoch)
+    train_step(model, device, train_loader, loss_function, optimizer)
+    validation_step(model, device, val_loader, loss_function)
--- unet/utils/load_data.py
+++ unet/utils/load_data.py
@@ -2,11 +2,53 @@ from torch.utils.data import Dataset
 import skimage
 import numpy as np
 import torch
+import sys
+import pandas as pd
+sys.path.append("../")
+from unet.augmentations.augmentations import (
+    Compose,
+    ToTensor,
+    LabelsToEdgesAndCentroids,
+    RandomContrastBrightness,
+    RandomGuassianBlur,
+    RandomGaussianNoise,
+    RandomRotate2D,
+    Flip,
+    RandomRot90,
+    RandomPoissonNoise,
+    ElasticDeform,
+    RandomScale,
+    Normalize
+)
 
 class MaddoxDataset(Dataset):
-    def __init__(self, data_csv, transform=None):
+    def __init__(self, data_csv, train_val="train"):
         self.data = data_csv
-        self.transform = transform
+        self.train_val = train_val
+        self.transforms = {
+            "train": 
+                Compose(
+                    [
+                        RandomContrastBrightness(p=0.5),
+                        Flip(p=0.5),
+                        RandomRot90(p=0.5),
+                        RandomGuassianBlur(p=0.5),
+                        RandomGaussianNoise(p=0.5),
+                        RandomPoissonNoise(p=0.5),
+                        ElasticDeform(sigma=5, points=1, p=0.5),
+                        LabelsToEdgesAndCentroids(centroid_pad=2),
+                        Normalize(),
+                        ToTensor()
+                    ]
+                ),
+            "val": 
+                Compose(
+                    [
+                        Normalize(),
+                        ToTensor()
+                    ]
+                )
+        }
 
     def __len__(self):
         return len(self.data)
@@ -14,17 +56,31 @@ class MaddoxDataset(Dataset):
     def __getitem__(self, idx):
         image = skimage.io.imread(self.data.iloc[idx, 0]).astype(np.float32)
         mask = skimage.io.imread(self.data.iloc[idx, 1]).astype(np.float32)
-        if self.transform:
+        if self.train_val == "train":
             sample = {'image': image, 'mask': mask}
-            data = self.transform(**sample)
-            # return data["image"], data["mask"]
+            data = self.transforms["train"](**sample)
+            return data
+        elif self.train_val == "val":
+            sample = {'image': image, 'mask': mask}
+            data = self.transforms["train"](**sample)
             return data
         else:
-            image = torch.from_numpy(image).astype(np.float32)
-            image = image[np.newaxis,...]
-            mask = torch.from_numpy(mask).astype(np.float32)
-            mask = mask[np.newaxis,...]
-            return image, mask
+            raise NotImplementedError
+
+    def collate_fn(self, data):
+        """Stack images and masks separately into batches
+        (batch, classes, D, H, W)"""
+        images = []
+        masks = []
+        for batch in data:
+            image = batch["image"]
+            mask = batch["mask"]
+            images.append(image)
+            masks.append(mask)
+
+        images = torch.stack(images, axis=0)
+        masks = torch.stack(masks, axis=0)
+        return images, masks
 
         
 
