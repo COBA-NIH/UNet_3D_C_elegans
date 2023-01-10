ae2ee24623c7bc58ce7390afd72252e2d76ad3ce
callum-jpg <callumtc.jpg@gmail.com>
2023-01-10 18:49:16 -0500

    network refactor

--- run_training.py
+++ run_training.py
@@ -8,8 +8,11 @@ import numpy as np
 from monai.losses import DiceLoss
 from monai.inferers import sliding_window_inference
 import logging
+from tqdm import tqdm
 
 from torch.utils.data import DataLoader, Dataset
+from torch.utils.tensorboard import SummaryWriter
+# http://localhost:6006
 
 import torch
 torch.cuda.empty_cache()
@@ -35,50 +38,79 @@ from unet.augmentations.augmentations import (
     RandomScale,
     Normalize
 )
-from unet.utils.load_data import MaddoxDataset
-from unet.networks.unet3d import UNet_3D
+from unet.utils.load_data import MaddoxDataset, RandomData
+from unet.networks.unet3d import UNet3D
+from unet.utils.loss import BCEDiceLoss
+
+writer = SummaryWriter("UNet3D_Maddox")
 
 load_csv = pd.read_csv("patch_data/load_data_training.csv")
 
 train_dataset, val_dataset = sklearn.model_selection.train_test_split(load_csv, test_size=0.2)
 
-train_ds = MaddoxDataset(
-    data_csv=train_dataset,
+# train_ds = MaddoxDataset(
+#     data_csv=train_dataset,
+#     train_val="train"
+# )
+
+# val_ds = MaddoxDataset(
+#     data_csv=val_dataset,
+#     train_val="val"
+# )
+
+
+train_ds = RandomData(
+    data_shape=(1, 4, 32, 32), 
+    dataset_size=20,
+    num_classes=2,
     train_val="train"
 )
 
-val_ds = MaddoxDataset(
-    data_csv=val_dataset,
+val_ds = RandomData(
+    data_shape=(1, 4, 32, 32), 
+    dataset_size=5,
+    num_classes=2,
     train_val="val"
 )
 
-train_loader = DataLoader(train_ds, batch_size=4, shuffle=True, collate_fn=train_ds.collate_fn)
+train_loader = DataLoader(train_ds, batch_size=1, shuffle=True, collate_fn=train_ds.collate_fn)
 
 # Don't shuffle validation so you can see how predictions improve over time
-val_loader = DataLoader(val_ds, batch_size=4, shuffle=False, collate_fn=val_ds.collate_fn)
+val_loader = DataLoader(val_ds, batch_size=1, shuffle=False, collate_fn=val_ds.collate_fn)
 
 def train_step(
     model,
     device,
     data_loader,
     loss_fn,
-    optimizer
+    optimizer,
+    epoch=None
 ):
+    train_loss = 0.0
+    train_n = 0
     model.train()
-    for i, (X, y) in enumerate(data_loader):
+    for i, (X, y) in enumerate(tqdm(data_loader)):
         # Put the image and the mask on the device
         X, y = X.to(device), y.to(device)
-        # Clear gradients from the optimizer
-        optimizer.zero_grad()
+
         # Run a prediction
         prediction = model(X)
-        print(f"Prediction shape: {prediction.shape} --- Target shape: {y.shape}")
         # Find the loss of the prediction when compared to the GT
         loss = loss_fn(prediction, y)
+
+        # Clear gradients from the optimizer
+        optimizer.zero_grad()
         # Using this loss, calculate the gradients of loss for all parameters
         loss.backward()
         # Update the the parameters of the model
         optimizer.step()
+
+        # Gather the loss
+        # X.size(0) is the batch size
+        train_loss += loss.item() * X.size(0)
+        # Increment the total number of training samples based on the batch size
+        # Used for calculating average metrics later
+        train_n += X.size(0)
 
         # Sigmoid scales values between 0-1
         probability = prediction.sigmoid().detach().cpu().numpy().flatten()
@@ -86,18 +118,23 @@ def train_step(
 
         # roc = sklearn.metrics.roc_auc_score(y, probability, labels=[0, 1], multi_class="ovo")
 
-        # Assume that correct predictions are above threshold
-        # Use correctly classified pixels to calculate precision, recall, accuracy and f1
+        # Convert probabilities into binary predictions
         th_prediction = (probability > 0.5)
-        precision = sklearn.metrics.precision_score(y, th_prediction, labels=[0, 1])
-        recall = sklearn.metrics.recall_score(y, th_prediction, labels=[0, 1])
+        precision = sklearn.metrics.precision_score(y, th_prediction)
+        recall = sklearn.metrics.recall_score(y, th_prediction)
         accuracy = sklearn.metrics.accuracy_score(y, th_prediction)
-        f1 = sklearn.metrics.f1_score(y, th_prediction, labels=[0, 1])
+        f1 = sklearn.metrics.f1_score(y, th_prediction)
 
-        logger.log(
-            level=logging.INFO, 
-            msg=f"Train step: [{i} of {len(data_loader)}] Loss: {loss.item()}, Acc: {accuracy}, Prec: {precision}, Recall: {recall}, F1: {f1}"
-            )
+        writer.add_scalar("Train/Loss", loss, epoch+1)
+        writer.add_scalar("Train/Accuracy", accuracy, epoch+1)
+        writer.add_scalar("Train/Precision", precision, epoch+1)
+        writer.add_scalar("Train/Recall", recall, epoch+1)
+        writer.add_scalar("Train/F1", f1, epoch+1)
+
+        # logger.log(
+        #     level=logging.INFO, 
+        #     msg=f"Train step: [{i + 1} of {len(data_loader)}] Loss: {loss.item()}, Acc: {accuracy}, Prec: {precision}, Recall: {recall}, F1: {f1}"
+        #     )
 
         # print(
         #     f"""
@@ -114,32 +151,39 @@ def validation_step(
     model,
     device,
     data_loader,
-    loss_fn
+    loss_fn,
+    epoch=None
 ):
     model.eval()
     with torch.no_grad():
         for i, (X, y) in enumerate(data_loader):
             X, y = X.to(device), y.to(device)
             prediction = model(X)
-            print(f"Prediction shape: {prediction.shape} --- Target shape: {y.shape}")
+            # print(f"Prediction shape: {prediction.shape} --- Target shape: {y.shape}")
             loss = loss_fn(prediction, y)
 
             probability = prediction.sigmoid().detach().cpu().numpy().flatten()
-            y = y.long().cpu().detach().numpy().flatten()
+            y = y.long().detach().cpu().numpy().flatten()
 
             # roc = sklearn.metrics.roc_auc_score(y, probability, labels=[0, 1], multi_class="ovo")
-            
+
+            # Convert probabilities into binary predictions
             th_prediction = (probability > 0.5)
-            precision = sklearn.metrics.precision_score(y, th_prediction, labels=[0, 1])
-            recall = sklearn.metrics.recall_score(y, th_prediction, labels=[0, 1])
+            precision = sklearn.metrics.precision_score(y, th_prediction)
+            recall = sklearn.metrics.recall_score(y, th_prediction)
             accuracy = sklearn.metrics.accuracy_score(y, th_prediction)
-            f1 = sklearn.metrics.f1_score(y, th_prediction, labels=[0, 1])
+            f1 = sklearn.metrics.f1_score(y, th_prediction)
 
+            writer.add_scalar("Validation/Loss", loss, epoch+1)
+            writer.add_scalar("Validation/Accuracy", accuracy, epoch+1)
+            writer.add_scalar("Validation/Precision", precision, epoch+1)
+            writer.add_scalar("Validation/Recall", recall, epoch+1)
+            writer.add_scalar("Validation/F1", f1, epoch+1)
 
-            logger.log(
-                level=logging.INFO, 
-                msg=f"Valid step: [{i} of {len(data_loader)}] Loss: {loss.item()}, Acc: {accuracy}, Prec: {precision}, Recall: {recall}, F1: {f1}"
-                )
+            # logger.log(
+            #     level=logging.INFO, 
+            #     msg=f"Valid step: [{i + 1} of {len(data_loader)}] Loss: {loss.item()}, Acc: {accuracy}, Prec: {precision}, Recall: {recall}, F1: {f1}"
+            #     )
 
             # print(
             #     f"""
@@ -152,15 +196,16 @@ def validation_step(
             #     """
             # )
 
-max_epochs = 2
+max_epochs = 100
 
-device = "cuda"
-model = UNet_3D(
-    in_channels=1, 
-    out_channels=2
+device = "cpu"
+model = UNet3D(
+    input_channels=1, 
+    num_classes=2,
+    network_depth=2,
+    activation="sigmoid"
     ).to(device)
 
-logger = logging.getLogger('training')
 logger = logging.getLogger("training")
 log_outfile = logging.FileHandler("experiment.log")
 log_outfile.setLevel(logging.DEBUG)
@@ -170,13 +215,18 @@ if torch.cuda.device_count() > 1:
     print("Running on multiple GPUs")
     model = nn.DataParallel(model)
 
-loss_function = DiceLoss(smooth_nr=0, smooth_dr=1e-5, squared_pred=True, to_onehot_y=False, sigmoid=False)
+loss_function = BCEDiceLoss()
+# loss_function = DiceLoss(smooth_dr=1e-5, squared_pred=True, to_onehot_y=False, sigmoid=False)
 optimizer = torch.optim.Adam(model.parameters(), 1e-4, weight_decay=1e-5)
 
 for epoch in range(max_epochs):
-    print("epoch:", epoch)
-    train_step(model, device, train_loader, loss_function, optimizer)
-    validation_step(model, device, val_loader, loss_function)
+    logger.log(
+        level=logging.INFO, 
+        msg=f"----- Epoch: {epoch + 1} -----"
+        )
+    train_step(model, device, train_loader, loss_function, optimizer, epoch=epoch)
+    validation_step(model, device, val_loader, loss_function, epoch=epoch)
+    
 
 
 
--- unet/utils/load_data.py
+++ unet/utils/load_data.py
@@ -21,6 +21,95 @@ from unet.augmentations.augmentations import (
     Normalize
 )
 
+class RandomData(Dataset):
+    """
+    The goal of this class is to generate some small,
+    random images for testing training. Thus, you can
+    test locally on a CPU before running elsewhere. 
+    """
+    def __init__(self, dataset_size:int, data_shape:tuple, num_classes:int, train_val="train"):
+            self.data_shape = data_shape
+            self.dataset_size = dataset_size
+            self.num_classes = num_classes
+            self.X, self.y = self.build_data(self.dataset_size, self.data_shape, self.num_classes)
+            self.train_val = train_val
+            self.transforms = {
+                "train": 
+                    Compose(
+                        [
+                            RandomContrastBrightness(p=0.5),
+                            Flip(p=0.5),
+                            RandomRot90(p=0.5),
+                            RandomGuassianBlur(p=0.5),
+                            RandomGaussianNoise(p=0.5),
+                            RandomPoissonNoise(p=0.5),
+                            # ElasticDeform(sigma=5, points=1, p=0.5), 
+                            Normalize(),
+                            ToTensor()
+                        ]
+                    ),
+                "val": 
+                    Compose(
+                        [
+                            Normalize(),
+                            ToTensor()
+                        ]
+                    )
+            }
+
+    def __len__(self):
+        return self.dataset_size
+
+    def build_data(self, data_size: int, data_shape: tuple, num_classes: int):
+        assert len(data_shape) == 4, "data_shape must have shape (channels, depth, height, width)"
+        channels, depth, height, width = data_shape
+        torch.manual_seed(42)
+        data_X = []
+        data_y = []
+        for i in range(data_size):
+            # dtype float32 for input pixels
+            data_X.append(
+                np.random.rand(channels, depth, height, width).astype(np.float32)
+            )
+            # dtype int64 since class predictions are ints
+            # max = 2 since randint requires it to be one above max. Unsure why
+            data_y.append(
+                np.random.randint(0, 2, (num_classes, depth, height, width)).astype(np.int64)
+            )
+        print(data_X[1].shape, data_y[1].shape)
+        return data_X, data_y
+
+    def __getitem__(self, idx):
+        X = self.X[idx]
+        y = self.y[idx]
+        if self.train_val == "train":
+            sample = {'image': X, 'mask': y}
+            data = self.transforms["train"](**sample)
+            return data
+        elif self.train_val == "val":
+            sample = {'image': X, 'mask': y}
+            data = self.transforms["train"](**sample)
+            return data
+        else:
+            raise NotImplementedError
+    
+    def collate_fn(self, data):
+        """Stack images and masks separately into batches
+        (batch, classes, D, H, W)"""
+        images = []
+        masks = []
+        for batch in data:
+            image = batch["image"]
+            mask = batch["mask"]
+            images.append(image)
+            masks.append(mask)
+
+        images = torch.stack(images, axis=0)
+        masks = torch.stack(masks, axis=0)
+        return images, masks
+
+
+
 class MaddoxDataset(Dataset):
     def __init__(self, data_csv, train_val="train"):
         self.data = data_csv
