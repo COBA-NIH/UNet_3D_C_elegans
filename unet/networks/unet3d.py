ef9b4562b67a2b54ff22910c7d807ba1ce2ef1e8
callum-jpg <callumtc.jpg@gmail.com>
2023-01-17 09:23:23 -0500

    adding some additional settings

--- unet/networks/unet3d.py
+++ unet/networks/unet3d.py
@@ -1,26 +1,51 @@
 import torch.nn as nn
 import torch
 
+from typing import Literal
+
+
 class ConvBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, use_batch_norm=True, conv_kernel_size=3):
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        conv_kernel_size=3,
+        normalization: Literal[None, "group", "batch"] = "batch",
+        num_groups=8,
+    ):
         super().__init__()
         # Calculate padding based on kernel size
         # Thus, the decoder path does not require cropping before concatenation
         # Though, if the conv_kernel_size isn't divisible, then will throw an error
         self.padding = (conv_kernel_size - 1) // 2
-        self.conv1 = nn.Conv3d(in_channels, out_channels, kernel_size=conv_kernel_size, padding=self.padding)
+        self.conv1 = nn.Conv3d(
+            in_channels,
+            out_channels,
+            kernel_size=conv_kernel_size,
+            padding=self.padding,
+        )
         self.relu = nn.ReLU()
-        self.conv2 = nn.Conv3d(out_channels, out_channels, kernel_size=conv_kernel_size, padding=self.padding)
+        self.conv2 = nn.Conv3d(
+            out_channels,
+            out_channels,
+            kernel_size=conv_kernel_size,
+            padding=self.padding,
+        )
 
+        # Find normalization method
+        if normalization == "group":
+            norm = nn.GroupNorm(num_groups, out_channels)
+        elif normalization == "batch":
+            norm = nn.BatchNorm3d(out_channels)
+        else:
+            norm = nn.Identity()
         # Create layers
         layers = []
         layers.append(self.conv1)
-        if use_batch_norm:
-            layers.append(nn.BatchNorm3d(out_channels))
+        layers.append(norm)
         layers.append(self.relu)
         layers.append(self.conv2)
-        if use_batch_norm:
-            layers.append(nn.BatchNorm3d(out_channels))
+        layers.append(norm)
         layers.append(self.relu)
 
         self.block = nn.Sequential(*layers)
@@ -28,12 +53,22 @@ class ConvBlock(nn.Module):
     def forward(self, x):
         return self.block(x)
 
+
 def calculate_feature_channels(initial_feature, multiplier, network_depth):
     """Calculate progressive feature channels"""
     return [initial_feature * multiplier ** k for k in range(network_depth)]
 
+
 class Encoder(nn.Module):
-    def __init__(self, input_channels: int, feature_map: list[int], use_batch_norm: bool, dropout_probability: float, conv_kernel_size: int):
+    def __init__(
+        self,
+        input_channels: int,
+        feature_map: list[int],
+        normalization,
+        num_groups,
+        dropout_probability: float,
+        conv_kernel_size: int,
+    ):
         super().__init__()
         self.dropout_probability = dropout_probability
         # Build the encoder features. Copy to prevent in-place insert
@@ -41,11 +76,21 @@ class Encoder(nn.Module):
         self.encoder_features.insert(0, input_channels)
         # Build the encoder blocks from feature_map
         self.encoder_blocks = nn.ModuleList(
-            [ConvBlock(self.encoder_features[i], self.encoder_features[i+1], use_batch_norm, conv_kernel_size=conv_kernel_size) for i in range(len(self.encoder_features) - 1)])
+            [
+                ConvBlock(
+                    self.encoder_features[i],
+                    self.encoder_features[i + 1],
+                    normalization=normalization,
+                    num_groups=num_groups,
+                    conv_kernel_size=conv_kernel_size,
+                )
+                for i in range(len(self.encoder_features) - 1)
+            ]
+        )
         self.pool = nn.MaxPool3d(kernel_size=2)
         # During model.eval(), dropout will become an identity module (ie. do nothing)
         self.dropout = nn.Dropout3d(p=self.dropout_probability)
-    
+
     def forward(self, x):
         features = []
         for block in self.encoder_blocks:
@@ -58,19 +103,41 @@ class Encoder(nn.Module):
             x = self.dropout(x)
         return features
 
+
 class Decoder(nn.Module):
-    def __init__(self, feature_map: list[int], use_batch_norm: bool, dropout_probability: float, conv_kernel_size: int):
+    def __init__(
+        self,
+        feature_map: list[int],
+        normalization,
+        num_groups,
+        dropout_probability: float,
+        conv_kernel_size: int,
+    ):
         super().__init__()
         self.dropout_probability = dropout_probability
         self.feature_map = feature_map
         self.up_conv = nn.ModuleList(
-            [nn.ConvTranspose3d(feature_map[i], feature_map[i+1], kernel_size=2, stride=2) for i in range(len(feature_map) - 1)]
-            )
+            [
+                nn.ConvTranspose3d(
+                    feature_map[i], feature_map[i + 1], kernel_size=2, stride=2
+                )
+                for i in range(len(feature_map) - 1)
+            ]
+        )
         self.decoder_block = nn.ModuleList(
-            [ConvBlock(feature_map[i], feature_map[i+1], use_batch_norm, conv_kernel_size=conv_kernel_size) for i in range(len(feature_map) - 1)]
+            [
+                ConvBlock(
+                    feature_map[i],
+                    feature_map[i + 1],
+                    normalization=normalization,
+                    num_groups=num_groups,
+                    conv_kernel_size=conv_kernel_size,
+                )
+                for i in range(len(feature_map) - 1)
+            ]
         )
         self.dropout = nn.Dropout3d(p=self.dropout_probability)
-        
+
     def forward(self, x, encoder_features):
         for i in range(len(self.feature_map) - 1):
             # Expansive path with ConvTranspose3d
@@ -81,6 +148,7 @@ class Decoder(nn.Module):
             x = self.dropout(x)
         return x
 
+
 class UNet3D(nn.Module):
     def __init__(
         self,
@@ -89,26 +157,43 @@ class UNet3D(nn.Module):
         initial_feature=64,
         feature_multiplier=2,
         network_depth=4,
-        use_batch_norm=True,
+        normalization: Literal[None, "group", "batch"] = "group",
+        num_groups: int = 8,
         conv_kernel_size=3,
         dropout_probability=0,
-        activation=None
-        ):
+        activation=None,
+    ):
         super().__init__()
-        self.feature_map = calculate_feature_channels(initial_feature, feature_multiplier, network_depth)
-        self.encoder = Encoder(input_channels, self.feature_map, use_batch_norm=use_batch_norm, dropout_probability=dropout_probability, conv_kernel_size=conv_kernel_size)
-        self.decoder = Decoder(self.feature_map[::-1], use_batch_norm=use_batch_norm, dropout_probability=dropout_probability, conv_kernel_size=conv_kernel_size)
+        self.feature_map = calculate_feature_channels(
+            initial_feature, feature_multiplier, network_depth
+        )
+        self.encoder = Encoder(
+            input_channels,
+            self.feature_map,
+            normalization=normalization,
+            num_groups=num_groups,
+            dropout_probability=dropout_probability,
+            conv_kernel_size=conv_kernel_size,
+        )
+        self.decoder = Decoder(
+            self.feature_map[::-1],
+            normalization=normalization,
+            num_groups=num_groups,
+            dropout_probability=dropout_probability,
+            conv_kernel_size=conv_kernel_size,
+        )
         # Output will take the lowest decoder features and output the desired number of classess
         out_layers = []
         out_layers.append(nn.Conv3d(self.feature_map[0], num_classes, kernel_size=1))
+
         if activation is not None:
             if activation.casefold() == "softmax":
                 out_layers.append(nn.Softmax(dim=1))
             elif activation.casefold() == "sigmoid":
                 out_layers.append(nn.Sigmoid())
+        
         self.head = nn.Sequential(*out_layers)
 
-    
     def forward(self, x):
         # Run the encoder
         encoder_features = self.encoder(x)
