82735dbea693b7d7fbed644faf96a5f516d534fe
callum-jpg <callumtc.jpg@gmail.com>
2023-02-13 10:00:08 -0500

    edge wmap augmentations, patched data loading

--- run_training_template.py
+++ run_training_template.py
@@ -9,7 +9,7 @@ from unet.networks.unet3d import UNet3D
 from unet.networks.unet3d import SingleConv
 # from unet.networks.unet3d import UnetModel
 import unet.augmentations.augmentations as aug
-from unet.utils.loss import WeightedBCELoss, WeightedBCEDiceLoss
+from unet.utils.loss import WeightedBCELoss, WeightedBCEDiceLoss, BCEDiceLoss, BCEDiceLossAlt
 from unet.utils.trainer import RunTraining
 import argparse
 import unet.utils.data_utils as utils
@@ -18,7 +18,7 @@ import unet.utils.data_utils as utils
 parser = argparse.ArgumentParser(description="3DUnet Training")
 
 # nargs="?" required to fall back to default if no arg provided
-parser.add_argument("data", nargs="?", default="patch_data/load_data_training.csv")
+parser.add_argument("data", nargs="?")
 parser.add_argument("--batch", nargs="?", default=4, type=int)
 parser.add_argument("--epochs", nargs="?", default=10, type=int)
 parser.add_argument("--workers", nargs="?", default=4, type=int)
@@ -27,22 +27,26 @@ parser.add_argument("--dummy", action="store_true")  # Use dummy data
 train_transforms = [
     aug.RandomContrastBrightness(p=0.5),
     aug.Flip(p=0.5),
-    aug.RandomRot90(p=0.5),
+    aug.RandomRot90(p=0.5, channel_axis=0),
     aug.RandomGuassianBlur(p=0.5),
     aug.RandomGaussianNoise(p=0.5),
     aug.RandomPoissonNoise(p=0.5),
-    aug.ElasticDeform(sigma=10, p=0.5),
-    aug.EdgesAndCentroids(iterations=3, mode="inner"),
+    aug.ElasticDeform(sigma=10, p=0.5, channel_axis=0, mode="mirror"),
+    aug.LabelsToEdges(connectivity=2),
+    aug.EdgeMaskWmap(),
+    # aug.BlurMasks(sigma=2),
     aug.Normalize(),
     aug.ToTensor()
 ]
 val_transforms = [
-    aug.EdgesAndCentroids(iterations=3, mode="inner"),
+    aug.LabelsToEdges(connectivity=2),
+    aug.EdgeMaskWmap(),
+    # aug.BlurMasks(sigma=2),
     aug.Normalize(),
     aug.ToTensor()
 ]
 
-targets=[["image"], ["mask"]]
+targets=[["image"], ["mask"], ["weight_map"]]
 
 def main():
     args = parser.parse_args()
@@ -66,15 +70,18 @@ def main_worker(args):
         )
     else:
         load_csv = pd.read_csv(args.data)
+        # Create the dataset (patches and weight maps, if required)
+        utils.create_patch_dataset(load_csv, (24, 200, 200), create_wmap=True, w0=3)
+        training_data = pd.read_csv("training_data.csv")
         train_dataset, val_dataset = sklearn.model_selection.train_test_split(
-            load_csv, test_size=0.2
+            training_data, test_size=0.2
         )
         print(
             f"loading data from: {args.data}. Train data of length {train_dataset.shape[0]} and val data of length {val_dataset.shape[0]}"
         )
-        train_ds = MaddoxDataset(data_csv=train_dataset, transforms=train_transforms, targets=targets, train_val="train", wmap=False)
+        train_ds = MaddoxDataset(data_csv=train_dataset, transforms=train_transforms, targets=targets, train_val="train", wmap=True)
 
-        val_ds = MaddoxDataset(data_csv=val_dataset, transforms=val_transforms, targets=targets, train_val="val", wmap=False)
+        val_ds = MaddoxDataset(data_csv=val_dataset, transforms=val_transforms, targets=targets, train_val="val", wmap=True)
 
     if torch.cuda.is_available():
         # Find fastest conv
@@ -113,12 +120,12 @@ def main_worker(args):
     #     dict_key="model_state_dict"
     # )
 
-    # model = utils.set_parameter_requires_grad(model, trainable=True)
-    model = utils.set_parameter_requires_grad(model, trainable=True, trainable_layer_name=["decoder"])
-    # Change the input layer
+    model = utils.set_parameter_requires_grad(model, trainable=True)
+
     model.encoders[0].basic_module.SingleConv1 = SingleConv(2, 16)
-    # Change the output layer
-    model.final_conv = nn.Conv3d(32, 3, kernel_size=1, stride=1)
+
+    # Replace final sigmoid
+    model.final_activation = nn.Identity()
 
     params_to_update = utils.find_parameter_requires_grad(model)
 
@@ -140,7 +147,7 @@ def main_worker(args):
     # as in `DataParallel` case
     # model = torch.nn.parallel.DistributedDataParallel(model)
 
-    loss_function = WeightedBCEDiceLoss(class_weights=[1,5,3], per_channel=True)
+    loss_function = WeightedBCEDiceLoss()
 
     # optimizer = torch.optim.Adam(model.parameters(), 1e-4, weight_decay=1e-5)
     optimizer = torch.optim.Adam(params_to_update, 1e-4, weight_decay=1e-5)
