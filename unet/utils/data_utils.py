8463de1d0b256e410cc0af2add44a7a6d45736c8
callum-jpg <callumtc.jpg@gmail.com>
2023-02-13 09:58:12 -0500

    adding binary wmap and patch dataset generation (more memory efficient to calc wmap at patch level)

--- unet/utils/data_utils.py
+++ unet/utils/data_utils.py
@@ -5,7 +5,10 @@ import torch
 import pathlib
 import torch.nn.functional as F
 import os
-import mahotas
+import monai
+import pandas as pd
+from tqdm import tqdm
+# import mahotas
 
 def generate_patches(image, patch_shape, stride_shape, unfold_dims=[0, 1, 2]):
     """Uses PyTorch unfolde to generate non-overlapping patches
@@ -183,6 +186,59 @@ def calculate_weight_map(gt_array, centroid_class_index=2, edge_class_index=1, l
     
     return wc_x
 
+def make_sequential(array):
+    unique = np.unique(array)
+    array = np.searchsorted(unique, array)
+    return array
+
+def calculate_binary_weight_map(labels, w0=10, sigma=5):
+    # Get total number of objects
+    labels = make_sequential(labels)
+    objects = np.unique(labels)
+    # print(len(objects))
+    objects = [obj for obj in objects if obj != 0]
+    if len(objects) > 150:
+        return labels
+    
+    # There exists multiple objects
+    if len(objects) > 1:
+        distances = np.zeros((len(objects), ) + labels.shape)
+        for i, region_id in enumerate(objects):
+            # Find the distance transforms for all objects except the iterable
+            distances[i,...] = scipy.ndimage.distance_transform_edt(labels.copy() != region_id)
+        """
+        Now, np.sort performs the magic of this function.
+
+        Above, you have defined the inverse distance transforms for each label independently. 
+        That is, each object will have a distance transform value of 0 and the non-object pixels
+        that are closest to the object of interest have increasing values
+
+        Across all of these object distance maps (axis 0), np.sort will find the lowest to 
+        highest distance values. Now, since each object was independently set to False for 
+        the distance transforms, all elements inside objects are 0 in the 0th (that is, (0, Z, X, Y)). 
+        So, for each object you can find distance to the nearest other object. 
+
+        The same is true for the 1st dimension (1, Z, X, Y), which is the distance to the border
+        of the second nearest object.
+
+        Analogy: You're at the bottom of a valley and you want to find the closest valley.
+        You climb up a small hill to get a better view and indeed, you spot another valley
+        on the other side of the small hill (0th). But now you want to find not a valley, but another
+        point that has the same height at the point you are at, so you ascend further up another 
+        small hill and now you have a better perspective and you look down around you
+        and see another point that was a similar height to your first ascent (1st). (ie. you're 
+        incrementally looking for local minima).
+        """
+        distances = np.sort(distances, axis = 0)
+        d1, d2 = distances[0,...], distances[1,...]
+
+        # Determine the separation border, as defined in https://arxiv.org/pdf/1505.04597.pdf
+        w = w0 * np.exp(-1 * ((d1 + d2) ** 2) / (2 * sigma ** 2))
+    else:
+        w = np.zeros_like(labels)
+    
+    return w
+
 
 def load_weights(model, weights_path, device, dict_key="state_dict"):
     weights = torch.load(weights_path, map_location=device)[dict_key]
@@ -217,23 +273,60 @@ def find_parameter_requires_grad(model):
             print("Training:", name)
     return params_to_update
 
-def watershed_from_edges(edges, threshold=0.5, erosion_footprint=skimage.morphology.ball(1)):
-    """Perform watershed from an edge map.
+# def watershed_from_edges(edges, threshold=0.5, erosion_footprint=skimage.morphology.ball(1)):
+#     """Perform watershed from an edge map.
     
-    Seed objects are calculated from regional minima in the edge map. These seeds are then eroded
+#     Seed objects are calculated from regional minima in the edge map. These seeds are then eroded
     
-    Resulting watershed segmentation will require some post-processing, like background removal."""
+#     Resulting watershed segmentation will require some post-processing, like background removal."""
 
-    th_edges = edges > threshold
+#     th_edges = edges > threshold
 
-    dist = scipy.ndimage.distance_transform_edt(th_edges)
+#     dist = scipy.ndimage.distance_transform_edt(th_edges)
 
-    seeds = mahotas.regmin(dist)
+#     seeds = mahotas.regmin(dist)
 
-    seeds = skimage.morphology.erosion(seeds, erosion_footprint)
+#     seeds = skimage.morphology.erosion(seeds, erosion_footprint)
 
-    seeds = scipy.ndimage.label(seeds)[0]
+#     seeds = scipy.ndimage.label(seeds)[0]
 
-    ws = skimage.segmentation.watershed(th_edges, seeds)
+#     ws = skimage.segmentation.watershed(th_edges, seeds)
 
-    return ws
+#     return ws
+
+def create_patch_dataset(load_data_csv, patch_size, save_dir=".", create_wmap=False, w0=10, sigma=5):
+    patch_iter = monai.data.PatchIter(patch_size, mode="reflect")
+    if create_wmap:
+        columns = ["image", "mask", "weight_map"]
+    else:
+        columns = ["image", "mask"]
+    output_data_csv = pd.DataFrame(columns=columns)
+    os.makedirs("./patch_images", exist_ok=True), os.makedirs("./patch_masks", exist_ok=True)
+    if create_wmap:
+        os.makedirs("./patch_weight_map", exist_ok=True)
+
+    for i in range(len(load_data_csv)):
+        image = skimage.io.imread(load_data_csv.iloc[i, 0])
+        mask = skimage.io.imread(load_data_csv.iloc[i, 1])
+        mask = make_sequential(mask)
+        mask = np.expand_dims(mask, 0) # Patching requires (C, spatial)
+        weight_map = mask.copy() # Will calculate actual weight map later
+        if create_wmap:
+            # weight_map = calculate_binary_weight_map(mask, w0=w0, sigma=sigma)
+            for ind, (img, msk, wmp) in enumerate(zip(patch_iter(image), patch_iter(mask), patch_iter(weight_map))):
+                img_fp, mask_fp, weight_map_fp = f"./patch_images/image_patch_{i}_{ind}.tiff", f"./patch_masks/mask_patch_{i}_{ind}.tiff", f"./patch_weight_map/weight_map_patch_{i}_{ind}.tiff"
+                skimage.io.imsave(img_fp, img[0], compression=("zlib", 1), check_contrast=False)
+                skimage.io.imsave(mask_fp, msk[0], compression=("zlib", 1), check_contrast=False)
+                wmp = calculate_binary_weight_map(wmp[0], w0=w0, sigma=sigma)
+                skimage.io.imsave(weight_map_fp, wmp, compression=("zlib", 1), check_contrast=False)
+                output_data_csv.loc[len(output_data_csv)] = [img_fp, mask_fp, weight_map_fp]
+        else:
+            print("No!")
+            for ind, (img, msk) in enumerate(zip(patch_iter(image), patch_iter(mask))):
+                img_fp, mask_fp = f"./patch_images/image_patch_{i}_{ind}.tiff", f"./patch_masks/mask_patch_{i}_{ind}.tiff"
+                skimage.io.imsave(img_fp, img[0], compression=("zlib", 1), check_contrast=False)
+                skimage.io.imsave(mask_fp, msk[0], compression=("zlib", 1), check_contrast=False)
+                output_data_csv.loc[len(load_datoutput_data_csva_csv)] = [img_fp, mask_fp]
+
+    output_data_csv.to_csv("training_data.csv", index=False)
+
